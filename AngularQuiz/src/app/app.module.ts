58f669219dd16cbef353d76b0ca7c175e269beda
Grzegorz Gardecki <gardecki.grzegorz@gmail.com>
2023-06-07 11:10:02 +0200

    First commit - Starting the project, creating the first components. Coming up with the rules for how the application will work

---
+++ AngularQuiz/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { QuestionComponent } from './question/question.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule, AppRoutingModule, RouterModule],
+  declarations: [AppComponent, WelcomeComponent, QuestionComponent],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
