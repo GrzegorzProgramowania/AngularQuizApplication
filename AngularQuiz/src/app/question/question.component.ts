c99d93202138a08bee91bb691c85261f651104b8
Grzegorz Gardecki <gardecki.grzegorz@gmail.com>
2023-06-28 11:21:24 +0200

    added localStorge, interpolation, json(with 20 sample questions) and service

--- AngularQuiz/src/app/question/question.component.ts
+++ AngularQuiz/src/app/question/question.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.css']
+  styleUrls: ['./question.component.css'],
 })
 export class QuestionComponent implements OnInit {
-
-  constructor() { }
+  public name: string = '';
+  constructor() {}
 
   ngOnInit(): void {
+    this.name = localStorage.getItem('name')!;
   }
-
 }
