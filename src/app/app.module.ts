73398fd59a3d0a76f0112166a2e8189685420f1b
Grzegorz Gardecki <gardecki.grzegorz@gmail.com>
2023-08-09 09:49:46 +0200

    I have updated the Welcome Component to prevent starting the quiz without providing a name. I have also implemented a safeguard that prevents proceeding to the next page by entering only white spaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ChangeBgDirective } from './change-bg.directive';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule, RouterModule],
-  declarations: [AppComponent, WelcomeComponent, QuestionComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    RouterModule,
+    HttpClientModule,
+  ],
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    QuestionComponent,
+    HeaderComponent,
+    ChangeBgDirective,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+// import { Router } from '@angular/router';
+
+// @Component({
+//   selector: 'app-welcome',
+//   templateUrl: './welcome.component.html',
+//   styleUrls: ['./welcome.component.css'],
+// })
+// export class WelcomeComponent implements OnInit {
+//   @ViewChild('name') nameKey!: ElementRef;
+
+//   constructor(private router: Router) {}
+
+//   ngOnInit(): void {}
+
+//   startQuiz() {
+//     localStorage.setItem('name', this.nameKey.nativeElement.value);
+//   }
+
+//   onEnter() {
+//     this.startQuiz();
+//     this.router.navigateByUrl('question');
+//   }
+// }
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  styleUrls: ['./welcome.component.css'],
 })
 export class WelcomeComponent implements OnInit {
+  @ViewChild('name') nameKey!: ElementRef;
+  isNameEmpty: boolean = true;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  startQuiz() {
+    localStorage.setItem('name', this.nameKey.nativeElement.value);
   }
 
+  onKeyUp(event: any) {
+    this.isNameEmpty = event.target.value.trim() === '';
+  }
+
+  onEnter() {
+    if (!this.isNameEmpty) {
+      this.startQuiz();
+      this.router.navigateByUrl('question');
+    }
+  }
 }
